cc56a19e3389c74d8e25aea80bc6ec4abbe3f253
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-06-14 00:11:24 -0700

    installed firebase backend

--- index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-
-const server = require('./server.js')
-
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => console.log(`API Running on ${port}`))
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 const Todo = props => {
@@ -6,12 +6,35 @@ const Todo = props => {
     const [todoName, setTodoName] = useState('')
     const [todoList, setTodoList] = useState([])
 
+
+    useEffect(() => {
+        axios.get('https://todohooks-3c2bf.firebaseio.com/todos.json')
+        .then(res => {
+            console.log(res)
+            const todoData = res.data
+            const todos = []
+            for(const key in todoData) {
+                todos.push({id: key, name: todoData[key].name})
+            }
+            setTodoList(todos)
+        })
+    }, [])
+
     const inputChangeHandler = (e) => {
         setTodoName(e.target.value)
     }
+    
 
     const todoAddHandler = () => {
         setTodoList(todoList.concat(todoName))
+        axios.post('https://todohooks-3c2bf.firebaseio.com/todos.json', {name: todoName})
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
     }
 
     return (
@@ -20,7 +43,7 @@ const Todo = props => {
             <button type = 'button' onClick = {todoAddHandler} >Add</button>
             <ul> 
                 {todoList.map((todo, i) => (
-                    <li key = {i}> {todo} </li>
+                    <li key = {todo.id}> {todo.name} </li>
                 ))}
             </ul>
         </React.Fragment>
